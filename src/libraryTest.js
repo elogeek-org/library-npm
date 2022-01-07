1208e970dc6d84faa2ab4deffe8425fa9b59571c
elogeek <elodiechristin@gmail.com>
2022-01-07 12:13:49 +0100

    library

---
+++ src/libraryTest.js
@@ -0,0 +1,10 @@
+function rainbowColor() {
+    console.log("merde");
+    let color = '';
+    for(let i = 0; i < 3; i++) {
+        let random = Math.floor(Math.random() * 256).toString(16);
+        color += (random.length ===  1 ? "0" + random : random);
+    }
+    console.log("youpi");
+    return "#" + color;
+}
